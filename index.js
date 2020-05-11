d820dec00c1a01903a1b147692c93f1b46913697
terraria <terraria@box.nc.rr.com>
2020-05-11 16:21:14 -0400

    adds save and exit commands

--- index.js
+++ index.js
@@ -13,39 +13,41 @@ bot.on("ready", () => {
 
 bot.on("message", async (message) => {
   if (message.content !== "!start") return;
-  const res = server.startServer();
+  const res = server.start();
   if (res.status) {
     message.channel.send(
       `failed with error code: ${res.status}, error message: ${res.content}`
     );
   } else {
     message.channel.send(`server is starting on ${await ip4()}:${config.port}`);
+    bot.user.setActivity('Terraria');
   }
 });
 
 bot.on("message", (message) => {
   if (!message.content.startsWith("!stop")) return;
 
-  const res = server.sendCommand("exit");
+  const res = server.exit();
   if (res.status) {
     message.channel.send(
       `failed with error code: ${res.status}, error message: ${res.content}`
     );
   } else {
-    message.channel.send(res.content);
+    message.channel.send("server is stopping");
+    bot.user.setActivity('Not Terraria');
   }
 });
 
 bot.on("message", (message) => {
-  if (!message.content.startsWith("!send")) return;
-  const command = message.content.substring(6);
-  const res = server.sendCommand(command);
+  if (!message.content.startsWith("!save")) return;
+
+  const res = server.save();
   if (res.status) {
     message.channel.send(
       `failed with error code: ${res.status}, error message: ${res.content}`
     );
   } else {
-    message.channel.send(res.content);
+    message.channel.send('server is saving');
   }
 });
 
--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const config = require("./config.json");
 
 let server = undefined;
 
-exports.startServer = () => {
+exports.start = () => {
   if (server) return { server, status: 0 };
   server = cp.spawn(config.serverPath, ["-config", config.serverConfigPath]);
   return { server, status: 0 };
 };
 
-exports.sendMessage = () => {
-  // something here to send messages
+exports.save = () => {
+  server.stdin.write("save\n");
+  return { status: 0 };
+};
+
+exports.exit = () => {
+  server.stdin.write("exit\n");
+  server = null
+  return { status: 0 };
 };
