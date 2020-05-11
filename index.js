cfc8030253365011678713e401cd6da9799ef8ef
terraria <terraria@box.nc.rr.com>
2020-05-11 15:06:10 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+const server = require("./server.js");
+
+const config = require("./config.json");
+
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log("ready");
+});
+
+bot.on("message", (message) => {
+  if (message.content !== "!start") return;
+  const res = server.startServer();
+  if (res.status) {
+    message.channel.send(
+      `failed with error code: ${res.status}, error message: ${res.content}`
+    );
+  } else {
+    message.channel.send("server is starting");
+  }
+});
+
+bot.on("message", (message) => {
+  if (!message.content.startsWith(":")) return;
+  const command = message.content.substring(1);
+  const res = server.sendCommand(command);
+  if (res.status) {
+    message.channel.send(
+      `failed with error code: ${res.status}, error message: ${res.content}`
+    );
+  } else {
+    message.channel.send(res.content);
+  }
+});
+
+console.log(config.token);
+bot.login(config.token);
---
+++ server.js
@@ -0,0 +1,16 @@
+const cp = require("child_process");
+const config = require("./config.json");
+
+let server = undefined;
+
+exports.startServer = () => {
+  if (server) return {server, status: 0};
+  server = cp.spawn(config.serverPath, ["-config", config.serverConfigPath]);
+  return {server, status:0};
+};
+
+exports.sendMessage = () => {
+  // something here to send messages
+}
+
+
